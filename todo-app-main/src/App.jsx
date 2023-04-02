9ec3169981e0a4ec973755b792d83b17c15c1bf7
Juan Sebastian Gonzalez <3393@holbertonstudents.com>
2023-04-01 22:38:39 -0500

    Setting up react application

---
+++ todo-app-main/src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div>
+      <h3>App</h3>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, animi!
+      </p>
+    </div>
+  );
+};
+export default App;
