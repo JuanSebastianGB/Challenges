14a7b1d3d5044a4fb4f8bfeb9bb0cf8fbd0ad30b
Juan Sebastian Gonzalez <3393@holbertonstudents.com>
2023-04-03 22:20:50 -0500

    Refactor: Refactor of filter component

--- todo-app-main/src/components/Filter/Filter.jsx
+++ todo-app-main/src/components/Filter/Filter.jsx
@@ -1,54 +1,55 @@
+import PropTypes from 'prop-types';
 import { useState } from 'react';
 import { useTasks } from '../../context/ContextProvider';
 import styles from './styles/Filter.module.css';
+
+const FilterItem = ({ filter, onClick, label }) => {
+  return (
+    <span className={filter ? styles.selected : ''} onClick={onClick}>
+      {label}
+    </span>
+  );
+};
+FilterItem.propTypes = {
+  filter: PropTypes.bool.isRequired,
+  onClick: PropTypes.func,
+  label: PropTypes.string.isRequired,
+};
 const Filter = () => {
   const { tasks, setFilteredTasks } = useTasks();
-  const [all, setAll] = useState(true);
-  const [active, setActive] = useState(false);
-  const [complete, setComplete] = useState(false);
+
+  const [filter, setFilter] = useState({
+    all: true,
+    active: false,
+    complete: false,
+  });
 
   const handleFilterAll = () => {
     setFilteredTasks(tasks);
-    setAll(true);
-    setActive(false);
-    setComplete(false);
+    setFilter({ all: true, active: false, complete: false });
   };
   const handleFilterActive = () => {
     setFilteredTasks(tasks.filter((task) => !task.completed));
-    setAll(false);
-    setActive(true);
-    setComplete(false);
+    setFilter({ all: false, active: true, complete: false });
   };
   const handleFilterComplete = () => {
     setFilteredTasks(tasks.filter((task) => task.completed));
-    setAll(false);
-    setActive(false);
-    setComplete(true);
+    setFilter({ all: false, active: false, complete: true });
   };
 
   return (
     <div className={styles.filter}>
-      <span
-        className={all ? styles.selected : ''}
-        name="all"
-        onClick={handleFilterAll}
-      >
-        All
-      </span>
-      <span
-        className={active ? styles.selected : ''}
-        name="active"
+      <FilterItem filter={filter.all} label="All" onClick={handleFilterAll} />
+      <FilterItem
+        filter={filter.active}
+        label="Active"
         onClick={handleFilterActive}
-      >
-        Active
-      </span>
-      <span
-        className={complete ? styles.selected : ''}
-        name="complete"
+      />
+      <FilterItem
+        filter={filter.complete}
+        label="Completed"
         onClick={handleFilterComplete}
-      >
-        Completed
-      </span>
+      />
     </div>
   );
 };
