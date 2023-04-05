8ca851542bfc6117a3f91b4f66156b3213a958c2
Juan Sebastian Gonzalez <3393@holbertonstudents.com>
2023-04-05 17:44:32 -0500

    Fix: Implementing data validation to ask for content when the task is being created, changing some colors to acomplish requirements and fixing favicon route

--- todo-app-main/src/components/Input/Input.jsx
+++ todo-app-main/src/components/Input/Input.jsx
@@ -6,9 +6,11 @@ import styles from './styles/Input.module.css';
 const Input = forwardRef((_, ref) => {
   const [inputData, setInputData] = useState('');
   const { tasks, setTasks, setFilteredTasks } = useTasks();
+  const [error, setError] = useState(false);
 
   const handleInsertTask = (e) => {
     if (e.key === 'Enter') {
+      if (inputData.length <= 5) return setError(true);
       const newData = [
         ...tasks,
         {
@@ -26,18 +28,34 @@ const Input = forwardRef((_, ref) => {
   useEffect(() => {
     ref.current.focus();
   }, []);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setError(false);
+    }, 2000);
+    return () => clearTimeout(timer);
+  }, [error]);
   return (
-    <div className={styles.input}>
-      <Check />
-      <input
-        type="text"
-        placeholder="Create new todo..."
-        value={inputData}
-        onChange={(e) => setInputData(e.target.value)}
-        onKeyDown={handleInsertTask}
-        ref={ref}
-      />
-    </div>
+    <>
+      <div className={styles.input}>
+        <Check />
+        <input
+          type="text"
+          placeholder="Create new todo..."
+          value={inputData}
+          onChange={(e) => setInputData(e.target.value)}
+          onKeyDown={handleInsertTask}
+          ref={ref}
+        />
+        <div
+          className={`${styles['container-error']} ${
+            error ? styles.error : ''
+          }`}
+        >
+          Required fill the the task
+        </div>
+      </div>
+    </>
   );
 });
 
