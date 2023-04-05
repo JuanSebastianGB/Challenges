bdfdebf42b71f8ae9c7d1b98c0b4db359f901770
Juan Sebastian Gonzalez <3393@holbertonstudents.com>
2023-04-04 22:07:16 -0500

    Fix: Fixing images src

--- todo-app-main/src/components/HeroImage/HeroImage.jsx
+++ todo-app-main/src/components/HeroImage/HeroImage.jsx
@@ -6,11 +6,11 @@ const HeroImage = () => {
       <picture className={styles['hero-image']}>
         <source
           media="(min-width: 600px)"
-          srcSet="../../../images/bg-desktop-light.jpg"
+          srcSet="src/images/bg-desktop-light.jpg"
         />
         <img
           className={styles['hero-image']}
-          src="images/bg-mobile-light.jpg"
+          src="src/images/bg-mobile-light.jpg"
           alt="Hero image"
         />
       </picture>
